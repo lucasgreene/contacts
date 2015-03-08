2ba843d283590d22d7221e5074fc5c6b1fded8e8
Lucas Adams <lucas_adams@brown.edu>
2015-03-08 18:21:30 -0400

    client stuff

--- client/Client.java
+++ client/Client.java
@@ -3,6 +3,8 @@ package contacts.client;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import contacts.addressbook.AddressBook;
 import contacts.parser.AddressbookNode;
@@ -15,18 +17,29 @@ public class Client {
 	
 	AddressBook book;
 	AddressbookNode abNode;
+	BufferedReader iStream;
 	public Client(String xmlFile) throws FileNotFoundException, TokenException {
 		BufferedReader reader = new BufferedReader(new FileReader( xmlFile));
 		XMLTokenizer t = new XMLTokenizer(reader);
 		Parser p = new Parser(t);
 		abNode = p.parseXMLPage();
 		book = abNode.toAddressbook();
+		iStream = new BufferedReader(new InputStreamReader(System.in));
 		
 	}
 	
-	public static void main(String[] args) {
+	public void takeInput() throws IOException {
+		String message = iStream.readLine();
+		while (message != null) {
+			InputParser.parse(message, this);
+		}
+		iStream.close();
+	}
+		
+	public static void main(String[] args) throws IOException {
 		try {
 			Client test = new Client("src/contacts/example.xml");
+			test.takeInput();
 			System.out.println(test.abNode.toString());
 		} catch (FileNotFoundException e){
 			System.out.println(e.getMessage());
