93cb569e063fd95cc132c03678197d2387238a2b
Russell Dodd <iDML@Russells-MacBook-Pro-2.local>
2015-03-13 00:37:01 -0400

    server

--- client/Client.java
+++ client/Client.java
@@ -22,7 +22,7 @@ import contacts.parser.XMLTokenizer;
 
 
 public class Client {
-	
+
 	AddressBook book;
 	AddressbookNode abNode;
 	BufferedReader iStream;
@@ -30,7 +30,7 @@ public class Client {
 	private static String host;
 	private static int port;
 	private Socket socket;
-	
+
 	public Client(String xmlFile, String host, int port) throws TokenException, UnknownHostException, IOException {
 		BufferedReader reader = new BufferedReader(new FileReader( xmlFile));
 		XMLTokenizer t = new XMLTokenizer(reader);
@@ -39,47 +39,47 @@ public class Client {
 		this.book = abNode.toAddressbook();
 		this.iStream = new BufferedReader(new InputStreamReader(System.in));
 		this.socket = new Socket(host, port);
-		
+
 	}
 
 	/*public void readNWrite(String command) throws IOException {
 
 		BufferedReader br1 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); 
-		
+
 	    InputStream is = socket.getInputStream();
 	    FileOutputStream fos = new FileOutputStream(xmlFile);
 	    BufferedOutputStream bos = new BufferedOutputStream(fos);
-		
+
 	    int in = System.in.read();
-		
+
 		while ( in != -1) {
-		
+
 			bw1.write(in);
 			in = System.in.read();
 		}
-		
+
 		bw1.flush();
-		
+
 		socket.shutdownOutput();
-		
+
 		int serveout= br1.read(); 
 		while (serveout != -1) {
-		
+
 		System.out.write((char) serveout);
 		serveout= br1.read(); 
 		}
-		
+
 		System.out.flush();
 		br1.close();
 
 	//	socket.shutdownInput();	
 	}
-	*/
+	 */
 	public void quit () {
 		quit = true;
 	}
-	
+
 	public void takeInput() throws IOException {
 		System.out.println("Enter a command:");
 		String message = iStream.readLine();
@@ -92,51 +92,51 @@ public class Client {
 		iStream.close();
 		System.out.println("Goodbye");
 	}
-	
+
 	public void push() throws IOException {
 		String xml = book.toXML();
 		BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
 		String header = "PUSH";
-		
+
 		bw1.write(header, 0, header.length());
-        bw1.write(xml, 0, xml.length());
-        
+		bw1.write(xml, 0, xml.length());
+
 		bw1.flush();
 		bw1.close();
-		
+
 	}
-	
+
 	public void pull() throws IOException, TokenException {
 		BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 		BufferedReader br1 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		
+
 		String header = "PULL";
-		
+
 		bw1.write(header, 0, header.length());
 		bw1.flush();
 		bw1.close();
-        String receive = br1.readLine();
-        if (receive.equals("ERROR")){
-        	System.out.println("Error pulling xml from server");
-        } else {
-        	BufferedReader reader = new BufferedReader(new FileReader( receive));
-    		XMLTokenizer t = new XMLTokenizer(reader);
-    		Parser p = new Parser(t);
-    		abNode = p.parseXMLPage();
-    		this.book = abNode.toAddressbook();
-        }
-		
+		String receive = br1.readLine();
+		if (receive.equals("ERROR")){
+			System.out.println("Error pulling xml from server");
+		} else {
+			BufferedReader reader = new BufferedReader(new FileReader( receive));
+			XMLTokenizer t = new XMLTokenizer(reader);
+			Parser p = new Parser(t);
+			abNode = p.parseXMLPage();
+			this.book = abNode.toAddressbook();
+		}
+
 	}
-	
+
 	public void queryPath() {
-		
+
 	}
-	
+
 	public void queryMutual() {
-		
+
 	}
-		
+
 	public static void main(String[] args) throws IOException {
 		try {
 			Client test = new Client("src/contacts/example.xml", "localhost", 1818);
@@ -144,7 +144,7 @@ public class Client {
 			AddressBook book = test.book;
 			Client test2 = new Client("src/contacts/test.xml", "localhost", 1818);
 			AddressBook book2 = test.book;
-			
+
 			System.out.println(book2.toXML().equals(book.toXML()));
 		} catch (FileNotFoundException e){
 			System.out.println(e.getMessage());
--- server/Server.java
+++ server/Server.java
@@ -29,7 +29,7 @@ public class Server {
 	private static String host;
 	private static int port;
 	private ServerSocket socket;
-	
+
 	public Server(String xmlFile, int port) throws TokenException, UnknownHostException, IOException {
 		BufferedReader reader = new BufferedReader(new FileReader( xmlFile));
 		XMLTokenizer t = new XMLTokenizer(reader);
@@ -38,42 +38,63 @@ public class Server {
 		this.book = abNode.toAddressbook();
 		this.iStream = new BufferedReader(new InputStreamReader(System.in));
 		this.socket = new ServerSocket(PORT);
-		
+
 	}
-	
+
 	public void takeInput() throws IOException {
 		while (true) {
 			Socket asock = socket.accept(); 
-			
+
 			BufferedReader br1 = new BufferedReader(new InputStreamReader(asock.getInputStream()));
 			BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(asock.getOutputStream()));
-			
+
 			String receive = br1.readLine();
 			if (receive.equals("PUSH\n")){
 				getPush(asock, br1);
 			} else if(receive.equals("PULL\n")){
-				
+				getPull(asock, br1);
 			} else if (receive.equals(" ")){
-				
+
 			}
-	        	
-	        }
+
+		}
 	}
-		
+
+
+	private void getPull(Socket asock, BufferedReader br1) {
+
+		BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(asock.getOutputStream()));
+		try {
+			String xml = book.toXML();
+
+			bw1.write(xml, 0, xml.length());
+
+			bw1.flush();
+			bw1.close();
+
+		} catch (NullPointerException e) {
+			String header = "ERROR";
+			bw1.write(header, 0, header.length());
+
+			bw1.flush();
+			bw1.close();
+		}
+
+	}
 
 	private void getPush(Socket asock, BufferedReader br1) {
 		BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(asock.getOutputStream()));
-		
+
 		try {
-		String receive = br1.readLine();
-		BufferedReader reader = new BufferedReader(new FileReader( receive));
-		XMLTokenizer t = new XMLTokenizer(reader);
-		Parser p = new Parser(t);
-		abNode = p.parseXMLPage();
-		this.book = abNode.toAddressbook();
-        String message = "OK";
-		
-		bw1.write(message, 0, message.length());
+			String receive = br1.readLine();
+			BufferedReader reader = new BufferedReader(new FileReader( receive));
+			XMLTokenizer t = new XMLTokenizer(reader);
+			Parser p = new Parser(t);
+			abNode = p.parseXMLPage();
+			this.book = abNode.toAddressbook();
+			String message = "OK";
+
+			bw1.write(message, 0, message.length());
 		} catch (IOException e) {
 			String message = "ERROR";
 			bw1.write(message, 0, message.length());
@@ -81,56 +102,56 @@ public class Server {
 	}
 
 	public void writeRead() throws IOException {
-		
-		while (true) {
-		Socket asock = socket.accept(); 
-		
-		BufferedReader br1 = new BufferedReader(new InputStreamReader(asock.getInputStream()));
-		BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(asock.getOutputStream())); 
-		
-		int in = asock.getInputStream().read();
-		while (in != -1) {
-		
-			bw1.write(in);
-			in = asock.getInputStream().read();
-		}
-		
-		bw1.flush();
-		
 
-		
-		int serveout= br1.read(); 
-		while (serveout != -1) {
-		
-			asock.getOutputStream().write((char) serveout);
-		serveout = br1.read(); 
+		while (true) {
+			Socket asock = socket.accept(); 
+
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(asock.getInputStream()));
+			BufferedWriter bw1 = new BufferedWriter(new OutputStreamWriter(asock.getOutputStream())); 
+
+			int in = asock.getInputStream().read();
+			while (in != -1) {
+
+				bw1.write(in);
+				in = asock.getInputStream().read();
+			}
+
+			bw1.flush();
+
+
+
+			int serveout= br1.read(); 
+			while (serveout != -1) {
+
+				asock.getOutputStream().write((char) serveout);
+				serveout = br1.read(); 
+			}
+
+			asock.getOutputStream().flush();
+			br1.close();
 		}
-		
-		asock.getOutputStream().flush();
-		br1.close();
-		}
-		
-	//	socket.shutdownInput();	
+
+		//	socket.shutdownInput();	
 	}
 
 
 
 
-	
-	
-	
+
+
+
 	public static void main(String[] args) {
-		
+
 		Server s;
 		try {
 			s = new Server(PORT);
 			s.writeRead();
 		} catch (IOException e) {
-		
+
 			e.printStackTrace();
 		} 
-		
-	
-		
+
+
+
 	}
 }
